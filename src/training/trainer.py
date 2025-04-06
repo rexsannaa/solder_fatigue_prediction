59d30c71e17bccf3d3edbd5d277cfd44979b6122
rexsannaa <mb33f206@office.stust.edu.tw>
2025-04-06 21:33:41 +0800

    a

--- src/training/trainer.py
+++ src/training/trainer.py
@@ -754,8 +754,8 @@ class Trainer:
         return {
             'loss': avg_loss,
             'metrics': metrics_values,
-            'predictions': all_predictions if all_predictions else None,
-            'targets': all_targets if all_targets else None,
+            'predictions': all_predictions if len(all_predictions) > 0 else None,
+            'targets': all_targets if len(all_targets) > 0 else None,
             'outputs': outputs_merged
         }
     
